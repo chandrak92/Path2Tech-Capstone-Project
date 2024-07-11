28391ba3c19a8f8c04cdc9336239c30bfd4a0b21
key2myboard <yrmn.rys@gmail.com>
2024-07-11 00:05:52 -0400

    working with html and css files placed in 'code here' directory

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from "./component/Homepage.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
