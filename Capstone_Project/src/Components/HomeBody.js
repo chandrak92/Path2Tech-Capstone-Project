70d599041a71f422cabbc5646934d1fb8f891f46
key2myboard <yrmn.rys@gmail.com>
2024-08-08 18:00:49 -0400

    added new image for Map page

--- Capstone_Project/src/Components/HomeBody.js
+++ Capstone_Project/src/Components/HomeBody.js
@@ -5,8 +5,8 @@ const HomeBody = () => {
     return (
         <section className="body">
             <h2>Codding Guppies Project</h2>
-            <p>Our web app is designed with the aim to empower users to easily adobt adobt a zero-waste lifestyle as it pertains to clothes and food.</p>
-            <p>We are here to encourage the public to take community-minded actions by proving easy access to how they can conviniently recycle unwanted clothing and food.</p>
+            <p>We aim to empower you to easily adobt a zero-waste lifestyle as it pertains to clothes, and other textiles, and food.</p>
+            <p>We encourage you to take community-minded actions by proving you easy access to how you can conviniently recycle unwanted belongings.</p>
             <p>Prevent reusable or recycable textiles from piling up on landfills.</p>
         </section>
 )
