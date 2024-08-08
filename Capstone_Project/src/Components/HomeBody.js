a53754d36e1c289da8de5125b23ea3646d55f2e1
key2myboard <yrmn.rys@gmail.com>
2024-08-08 16:39:50 -0400

    modified navbar and home styling

--- Capstone_Project/src/Components/HomeBody.js
+++ Capstone_Project/src/Components/HomeBody.js
@@ -8,9 +8,6 @@ const HomeBody = () => {
             <p>Our web app is designed with the aim to empower users to easily adobt adobt a zero-waste lifestyle as it pertains to clothes and food.</p>
             <p>We are here to encourage the public to take community-minded actions by proving easy access to how they can conviniently recycle unwanted clothing and food.</p>
             <p>Prevent reusable or recycable textiles from piling up on landfills.</p>
-            <ul>
-                <li className=""><NavLink to="/about">About</NavLink></li>
-            </ul>
         </section>
 )
 }
