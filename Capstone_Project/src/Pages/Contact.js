ba59e0ac4a895f1fcde4b0b62d19043d66cccb96
Simone <moores0929@gmail.com>
2024-08-07 23:08:31 -0400

    Updates to Quiz and Login/Logout functionality. Able to render NavBar dependent on user's loggedin status. Quiz rendering questions from quizarray.js. Updates made to contact UI, functionality still needed to be refactored. Quiz UI needs refactoring. Locator missing from push, will be pulled and then pushed to main branch once merge is completed.

--- Capstone_Project/src/Pages/Contact.js
+++ Capstone_Project/src/Pages/Contact.js
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import "../Style/contact.css"
+// import background from '../Assets/CanyonPhoto.jpg';
+import "../Style/contact.css";
 
 const Contact = () => {
     // let founderEmail= [["moores0929@gmail.com", "Simone"], ["","Chandra"], ["", "Yarmin"]];
@@ -25,57 +26,61 @@ const Contact = () => {
        
     // }
     return(
-        <section className="contact-container">
-            <div className="contact-head">
-                <h2>Contact Form</h2>
-                <p>If you are interested in our work, fill out this form below</p>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div
-                    onMouseEnter={() => setHovered(true)}
-                    onMouseLeave={() => setHovered(false)}
-                >
-                    {/* ternary operator to conditional style a way to confirm you've selected an email */}
-                    <ul className="contact-drop">
-                        <li className="founder-email">Contact Founder</li>
-                            <ul className={`dropdown-contact ${hovered ? "active-contact" : "hidden-contact" }`}>
-                                    <li className="founder" value="" onClick="">Chandra</li>
-                                    <li className="founder" value="moores0929@gmail.com" onCLick="">Simone</li>
-                                    <li className="founder" value="" onClick="">Yarmin</li>
-                            </ul>
-                    </ul>
+        <section className="contact-outer">
+            <section className="contact-container">
+                <div className="contact-head">
+                    <h2>Contact Form</h2>
+                    <p>If you are interested in our work, fill out this form below</p>
                 </div>
-                <label htmlFor="email">
-                    <p>Your Email</p>
-                    <input
-                    {...register("email", { required: true })}
-                    type="email" 
-                    id="email" 
-                    placeholder="Enter your email here"
-                    required
-                    />
-                </label>
-                <label htmlFor="subject">
-                    <p>Subject</p>
-                    <input 
-                    {...register("subject", { required: true })}
-                    type="text" 
-                    id="subject"
-                    placeholder="Enter your subject here"
-                    required
-                    />
-                </label>
-                <label htmlFor="body">
-                    <p>Body</p>
-                    <textarea
-                    {...register("body", { required: true })}
-                    id="body" 
-                    placeholder="Enter your message here"
-                    required
-                    />
-                </label>
-                <button type="submit">Send Email</button>
-            </form>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div
+                        onMouseEnter={() => setHovered(true)}
+                        onMouseLeave={() => setHovered(false)}
+                    >
+                        {/* ternary operator to conditional style a way to confirm you've selected an email */}
+                        <ul className="contact-drop">
+                            <li className="founder-email">Contact Founder</li>
+                                <ul className={`dropdown-contact ${hovered ? "active-contact" : "hidden-contact" }`}>
+                                        <li className="founder" value="" onClick="">Chandra</li>
+                                        <li className="founder" value="moores0929@gmail.com" onCLick="">Simone</li>
+                                        <li className="founder" value="" onClick="">Yarmin</li>
+                                </ul>
+                        </ul>
+                    </div>
+                    <div className="contact-input">
+                        <label htmlFor="email">
+                            <p>Your Email</p>
+                            <input
+                            {...register("email", { required: true })}
+                            type="email" 
+                            id="email" 
+                            placeholder="Enter your email here"
+                            required
+                            />
+                        </label>
+                        <label htmlFor="subject">
+                            <p>Subject</p>
+                            <input 
+                            {...register("subject", { required: true })}
+                            type="text" 
+                            id="subject"
+                            placeholder="Enter your subject here"
+                            required
+                            />
+                        </label>
+                        <label htmlFor="body">
+                            <p>Body</p>
+                            <textarea
+                            {...register("body", { required: true })}
+                            id="body" 
+                            placeholder="Enter your message here"
+                            required
+                            />
+                        </label>
+                        <button className="contact-button" type="submit">Send Email</button>
+                    </div>
+                </form>
+            </section>
         </section>
     )
 }
