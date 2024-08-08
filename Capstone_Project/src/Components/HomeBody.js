80f06fc6612d6852bdcce6e7f057304249053a4e
key2myboard <yrmn.rys@gmail.com>
2024-08-08 02:30:17 -0400

    Updated homepage content, deleted unnecessary logos

--- Capstone_Project/src/Components/HomeBody.js
+++ Capstone_Project/src/Components/HomeBody.js
@@ -4,8 +4,10 @@ import { NavLink } from "react-router-dom";
 const HomeBody = () => {
     return (
         <section className="body">
-            <h2>Main Content</h2>
-            <p>This is the main content of the homepage.</p>
+            <h2>Codding Guppies Project</h2>
+            <p>Our web app is designed with the aim to empower users to easily adobt adobt a zero-waste lifestyle as it pertains to clothes and food.</p>
+            <p>We are here to encourage the public to take community-minded actions by proving easy access to how they can conviniently recycle unwanted clothing and food.</p>
+            <p>Prevent reusable or recycable textiles from piling up on landfills.</p>
             <ul>
                 <li className=""><NavLink to="/about">About</NavLink></li>
             </ul>
